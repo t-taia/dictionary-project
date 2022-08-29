7dbde6d9280bea9baed1fcb24431512c57dd6215
t-taia <95856208+t-taia@users.noreply.github.com>
2022-08-29 12:46:09 +0200

    added rel="noreferrer"

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,7 +4,7 @@ export default function Phonetics(props) {
     console.log(props.phonetic);
     return (
         <div className="Phonetics">
-            <a href={props.phonetic.audio} target="_blank">
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
                 Listen
             </a>
             <br />
