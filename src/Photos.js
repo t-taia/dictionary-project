f949a1e36cdc9802decf86b1bbb53617e4e88105
t-taia <95856208+t-taia@users.noreply.github.com>
2022-08-30 15:31:58 +0200

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -4,15 +4,14 @@ import "./Photos.css";
 export default function Photos(props) {
     
     if (props.photos) {
-        console.log(props.photos);
         return (
             <section className="Photos">
               <div className="row"> 
                 {props.photos.map(function(photo, index) {
                     return (
                         <div className="col-3" key={index}>
-                            <a href={photo.src.original} rel="noreferrer" target="_blank">
-                              <img src={photo.src.tiny} className="img-fluid" />
+                            <a href={photo.src.original} target="_blank" rel="noreferrer" >
+                              <img src={photo.src.landscape} className="img-fluid" />
                             </a>
                         </div>
                     );
